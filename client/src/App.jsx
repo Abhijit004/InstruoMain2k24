496487621cbdbddc62e57646982cf1174210acae
Saral Gupta <saralgup.taxtc@gmail.com>
2024-12-10 14:27:21 +0530

    [BUILD]: Added Footer and Necessary Assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AllRoutes from "./Routes";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/footer";
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <div className="instruo-content">
-                <AllRoutes />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavBar />
+      <div className="instruo-content">
+        <AllRoutes />
+      </div>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;

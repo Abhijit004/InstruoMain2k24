6c5e0c2d96d5f34c97ca7f578321d1044c889812
Soumyajit <sona832004@gmail.com>
2024-12-11 22:04:24 +0530

    [UPDATE]: Upgraded custombutton to have both primary and secondary variants

--- client/src/components/CustomButton/CustomButton.jsx
+++ client/src/components/CustomButton/CustomButton.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { AntDesignOutlined } from "@ant-design/icons";
 import { Button, ConfigProvider } from "antd";
 import { createStyles } from "antd-style";
-const useStyle = createStyles(({ prefixCls, css }) => ({
+
+const useStylePrimary = createStyles(({ prefixCls, css }) => ({
     linearGradientButton: css`
         &.${prefixCls}-btn-primary:not([disabled]):not(.${prefixCls}-btn-dangerous) {
             border-width: 0;
@@ -13,7 +14,7 @@ const useStyle = createStyles(({ prefixCls, css }) => ({
 
             &::before {
                 content: "";
-                background: linear-gradient(135deg, #2D76FF, #50379B);
+                background: linear-gradient(135deg, var(--color-darkblue), var(--color-purple));
                 position: absolute;
                 inset: 0;
                 opacity: 1;
@@ -27,9 +28,39 @@ const useStyle = createStyles(({ prefixCls, css }) => ({
         }
     `,
 }));
-const CustomButton = ({text, ...props}) => {
-    const { styles } = useStyle();
-	console.log(styles)
+
+
+const useStyleSecondary = createStyles(({ prefixCls, css }) => ({
+    linearGradientButton: css`
+        &.${prefixCls}-btn-primary:not([disabled]):not(.${prefixCls}-btn-dangerous) {
+            border-width: 0;
+            color: var(--color-purple);
+
+            > span {
+                position: relative;
+            }
+
+            &::before {
+                content: "";
+                background: #fff;
+                position: absolute;
+                inset: 0;
+                opacity: 1;
+                transition: all 0.3s;
+                border-radius: inherit;
+            }
+
+            &:hover::before {
+                opacity: 0.5
+            }
+        }
+    `,
+}));
+
+
+const CustomButton = ({text, variant='primary', ...props}) => {
+    const { styles } = variant==='secondary'?useStyleSecondary():useStylePrimary();
+	// console.log(styles)
     return (
         <ConfigProvider
             button={{

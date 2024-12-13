0f3fb74dcfc3e0347fd0f4f5fe5d1bf447fc2365
Soumyajit <sona832004@gmail.com>
2024-12-13 16:20:49 +0530

    [BUILD]: Preparing the events page

---
+++ client/src/Screens/Events/DayRight.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./DayRight.css";
+import EventGroup from "../../components/EventGroup/EventGroup";
+import PageHeading from "../../components/PageHeading/PageHeading";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import Mesh from "../../components/Mesh/Mesh";
+
+const DayRight = () => {
+    return (
+        <div className="dayright">
+            <div className="content">
+                <EventGroup />
+                <div className="about-day">
+                    <Mesh />
+                    <PageHeading variant={"color"} text={"Day two"} />
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione excepturi minima animi
+                        odit, vitae dignissimos nisi expedita obcaecati commodi at sunt nostrum corporis tenetur amet
+                        iusto. Est, nam totam.
+                    </p>
+                    <CustomButton text={"Know more"} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DayRight;
--- client/src/Screens/Events/Events.jsx
+++ client/src/Screens/Events/Events.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Events.css"
 import EventsHero from './EventsHero'
+import DayLeft from './DayLeft'
+import DayRight from './DayRight'
 
 const Events = () => {
   return (
     <div className="events-page">
         <EventsHero />
+        <DayLeft day={'one'} />
+        <DayRight />
+        <DayLeft day={'three'} />
     </div>
   )
 }

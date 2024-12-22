33a1ae298cbebd2363a1c7424e93077780fb0c3c
Soumyajit <sona832004@gmail.com>
2024-12-22 12:05:19 +0530

    [BUGFIX]: Adding form changes to event-addition

--- client/src/Screens/SingleEvent/SingleEvent.jsx
+++ client/src/Screens/SingleEvent/SingleEvent.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./SingleEvent.css";
 
 const SingleEvent = () => {
-    const {eventID} = useParams();
+    const { eventID } = useParams();
     console.log(eventID);
 
-    return <div className="single-event"></div>;
+    const back = "/assets/groupfie.webp"
+
+    return (
+        <div className="single-event">
+            <div className="hero">
+                <div style={{
+                    background: `url("${back}")`
+                }} className="banner"></div>
+                <div className="heading gradient-bw">Cool event!</div>
+                <div className="byline">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempora nam dolorem natus quo
+                    consectetur velit. Nihil repellat laboriosam officiis.
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SingleEvent;

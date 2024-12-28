1f10d53eaa6d89bb68b8021be7241c223a77ccc9
DugarRishab <www.anitadugar9163@gmail.com>
2024-12-29 01:12:08 +0530

    [UPDATE]: updated buttons in events card

--- client/src/components/EventGroup/EventGroup.jsx
+++ client/src/components/EventGroup/EventGroup.jsx
@@ -17,32 +17,52 @@ const backdrop = (src) => {
 const Card = ({ rank, eventData, open, changeFocus }) => {
     console.log(formatTimestamp(eventData.startTime));
 
+    console.log("event doc: ", eventData.rulesDoc)
+
     const src = eventData.images[0].url;
     return (
-        <div onClick={() => changeFocus(rank)} className={`eventcard ${open ? "open" : ""}`}>
-            <div style={backdrop(src)} className="background"></div>
-            <div className="heading">{eventData.name}</div>
-            <div className="body">
-                <div className="head">{eventData.name}</div>
-                <div className="text">{eventData.description}</div>
-                <ButtonGroup style={{
-                    gap: "10px"
-                }}>
-                    <Link to={`/events/${eventData._id}`}>
-                        <CustomButton style={{fontWeight: 600}} variant="secondary" className="button" text={"Know more"} />
-                    </Link>
-                    <a href={`https://register.instruo.tech?event=${eventData._id}`}>
-                        <CustomButton style={{fontWeight: 600}} className="button" text={"Register"} />
-                    </a>
-                </ButtonGroup>
-                <div className="rating">
-                    {formatTimestamp(eventData.startTime)}
-                    <br />
-                    to {formatTimestamp(eventData.endTime)}
-                </div>
-            </div>
-        </div>
-    );
+		<div
+			onClick={() => changeFocus(rank)}
+			className={`eventcard ${open ? "open" : ""}`}
+		>
+			<div style={backdrop(src)} className="background"></div>
+			<div className="heading">{eventData.name}</div>
+			<div className="body">
+				<div className="head">{eventData.name}</div>
+				<div className="text">{eventData.description}</div>
+				<ButtonGroup
+					style={{
+						gap: "10px",
+					}}
+				>
+					{eventData.rulesDoc && (
+						<a href={eventData.rulesDoc}>
+							<CustomButton
+								style={{ fontWeight: 600 }}
+								variant="secondary"
+								className="button"
+								text={"Rulebook"}
+							/>
+						</a>
+					)}
+					{eventData.registrationUrl && (
+						<a href={eventData.registrationUrl}>
+							<CustomButton
+								style={{ fontWeight: 600 }}
+								className="button"
+								text={"Register"}
+							/>
+						</a>
+					)}
+				</ButtonGroup>
+				<div className="rating">
+					{formatTimestamp(eventData.startTime)}
+					<br />
+					to {formatTimestamp(eventData.endTime)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const genres = ["Fantasy", "Thriller", "Romance", "Comedy"];
@@ -52,7 +72,7 @@ const EventGroup = ({ events }) => {
     const changeFocus = (num) => {
         setCardNumber(() => num);
     };
-    console.log(events);
+    // console.log(events);
 
     return (
         <div className="eventbox-wrapper">

fda3fccc28772cd4471fc1a5b28e1811b84c4835
Soumyajit <sona832004@gmail.com>
2024-12-31 15:35:30 +0530

    [UPDATE]: Setting up singleEvent page for extracting backend data

--- client/src/Screens/SingleEvent/SingleEvent.jsx
+++ client/src/Screens/SingleEvent/SingleEvent.jsx
@@ -1,31 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./SingleEvent.css";
+import { CalendarOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import { getEventByID } from "../../services/api";
+import formatTimestamp from "../../services/FormatTime";
 
 const SingleEvent = () => {
     const { eventID } = useParams();
-    console.log(eventID);
+    const [bg, setBg] = useState(0);
+    const [eventDetail, setEventDetail] = useState(null);
+    const [bglist, setBglist] = useState(["/assets/groupfie.webp"]);
 
-    const back = "/assets/groupfie.webp"
+    // useEffect(() => {
+    //     const galleryInterval = setInterval(() => {
+    //         setBg(bg=>(bg+1)%bglist.length);
+    //     }, 5000);
+
+    //     return () => {
+    //         clearInterval(galleryInterval);
+    //     };
+    // }, []);
+
+    const getEventDetails = async () => {
+        try {
+            const res = await getEventByID(eventID);
+            setEventDetail(res.data)
+            console.log(res.data);
+            if(res){
+                const tempBg = bglist;
+                res.data.images.forEach((img)=>{
+                    if(img.type==='gallery'){
+                        tempBg.push(img.url);
+                    }
+                })
+                setBglist(tempBg)
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getEventDetails();
+    }, []);
 
     return (
         <div className="single-event">
             <div className="hero">
-                <div style={{
-                    background: `url("${back}")`
-                }} className="banner"></div>
-                <div className="heading gradient-bw">Cool event!</div>
+                <div
+                    style={{
+                        background: `url("${bglist[bg]}")`,
+                        transition: "allease 0.4s"
+                    }}
+                    className="banner"
+                ></div>
+                <div className="heading gradient-bw">{eventDetail?.name}</div>
                 <div className="time">
-                    10th Jan, 2025, 11:30 PM - 12th Jan, 2025, 11:45 PM
+                    <CalendarOutlined /> {eventDetail && formatTimestamp(eventDetail.startTime)} to {eventDetail && formatTimestamp(eventDetail.endTime)}
                 </div>
                 <div className="venue">
-                    Cool venue
-                </div>
-                <div className="byline">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti tempora nam dolorem natus quo
-                    consectetur velit. Nihil repellat laboriosam officiis.
+                    <EnvironmentOutlined /> {eventDetail?.venue}
                 </div>
             </div>
+
         </div>
     );
 };
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -16,3 +16,4 @@ export const createEvent = (payload) => api.post('/api/event', payload);
 export const googleAuth = (code) => api.get(`/api/v1/auth/google?code=${code}`);
 export const googleLogout = () => api.get(`/api/v1/auth/logout`);
 export const googleStatus = () => api.get(`/user/status`);
+export const getEventByID = (id) => api.get(`/api/event/${id}`);

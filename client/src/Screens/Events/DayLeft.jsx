bcfcbc1a3920c53bd678d1cb9815c84a628015bd
Soumyajit <sona832004@gmail.com>
2025-01-02 10:17:25 +0530

    Revert "[BUGFIX]: Fixing events page design"
    This reverts commit 9600355af081c7563308540e17d3c4d07cdd8ef5.

--- client/src/Screens/Events/DayLeft.jsx
+++ client/src/Screens/Events/DayLeft.jsx
@@ -6,25 +6,25 @@ import CustomButton from "../../components/CustomButton/CustomButton";
 import Mesh from "../../components/Mesh/Mesh";
 
 const DayLeft = ({ day, events, para }) => {
-    return (
-        <div className="dayleft">
-            <div className="content">
-                <div className="about-day">
-                    <Mesh
-                        style={{
-                            right: 0,
-                        }}
-                    />
-                    <PageHeading variant={"color"} text={`Day ${day}`} />
-                    <p>{para}</p>
-                    {/* <CustomButton text={"Know more"} /> */}
-                </div>
-                <div className="event-scroll">
-                    <EventGroup events={events} />
-                </div>
-            </div>
+  return (
+    <div className="dayleft">
+      <div className="content">
+        <div className="about-day">
+          <Mesh
+            style={{
+              right: 0,
+            }}
+          />
+          <PageHeading variant={"color"} text={`Day ${day}`} />
+          <p>{para}</p>
+          {/* <CustomButton text={"Know more"} /> */}
         </div>
-    );
+        <div className="event-scroll">
+          <EventGroup events={events} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DayLeft;

1fab3b447fd1be867cfa23a8ff63856b014254a9
Soumyajit <sona832004@gmail.com>
2025-01-02 10:25:14 +0530

    [UPDATE]: Adding react-counter to hypecards

--- client/src/Screens/Home/Hype.jsx
+++ client/src/Screens/Home/Hype.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hype.css";
 import HypeCard from "../../components/HypeCard/HypeCard";
 import { CalendarOutlined, SmileOutlined, SmileTwoTone, TrophyOutlined, UsergroupAddOutlined } from "@ant-design/icons";
+import Mesh from "../../components/Mesh/Mesh"
 
 const Hype = () => {
     return (
@@ -10,29 +11,31 @@ const Hype = () => {
                 Making The Hype <span>Real</span>
             </div>
             <div className="hype-stats">
+                <Mesh />
                 <HypeCard
-                    count={"6000+"}
+                    count={"6000"}
                     desc={"Crowds Growing with Excited Footfall"}
                     subtitle={"Footfall"}
                     color={"#C096DD"}
                     Icon={UsergroupAddOutlined}
                 />
                 <HypeCard
-                    count={"17+"}
+                    count={"17"}
                     desc={"High Satisfaction Among Our Sponsors"}
                     subtitle={"Sponsors' satisfaction"}
                     color={"#5FAFC1"}
                     Icon={SmileOutlined}
                 />
                 <HypeCard
-                    count={"23"}
+                    count={"20"}
                     desc={"Stay Updated on Upcoming Events"}
                     subtitle={"Events"}
                     color={"#549DF6"}
                     Icon={CalendarOutlined}
                 />
                 <HypeCard
-                    count={"800k+"}
+                    count={"800"}
+                    suffix={"k+"}
                     desc={"Exciting Prize Money Awaits the Winners"}
                     subtitle={"Prize Money"}
                     color={"#E7D368"}
